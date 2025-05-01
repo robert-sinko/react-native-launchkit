772a2771a44961023d9f3d369699c5ab84398371
robert <sinko.robert.t@gmail.com>
2025-05-01 13:28:29 +0300

    Install eslint, prettier and plugins.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import "./global.css"
+import "./global.css";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className='flex-1 items-center justify-center bg-white'>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
