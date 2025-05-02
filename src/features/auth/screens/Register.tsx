92dfb366b3ca2e4b1616fcc5428ed073d32832f3
robert <sinko.robert.t@gmail.com>
2025-05-02 12:39:43 +0300

    feat: implement registration form with validation and input fields

--- src/features/auth/screens/Register.tsx
+++ src/features/auth/screens/Register.tsx
@@ -1,10 +1,104 @@
+import Button from "../../../components/Button";
 import Text from "../../../components/Text";
-import { SafeAreaView } from "react-native";
+import TextInput from "../../../components/TextInput";
+import { Controller, useForm } from "react-hook-form";
+import { SafeAreaView, View } from "react-native";
 
 export default function RegisterScreen() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-slate-100">
-      <Text className="text-3xl">register here</Text>
+    <SafeAreaView className="flex-1 bg-slate-100">
+      <View className="p-6 pt-24">
+        <Text className="text-4xl">Welcome!</Text>
+        <View className="pb-1 pt-10">
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                placeholder="Enter your name"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+            name="name"
+          />
+        </View>
+        <View className="py-1">
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                placeholder="Enter your email"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+            name="email"
+          />
+        </View>
+        <View className="py-1">
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                placeholder="Enter your password"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                secureTextEntry={true}
+              />
+            )}
+            name="password"
+          />
+        </View>
+        <View className="py-1">
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                placeholder="Confirm your password"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                secureTextEntry={true}
+              />
+            )}
+            name="confirmPassword"
+          />
+        </View>
+        <View className="pt-10">
+          <Button
+            title="Register"
+            onPress={handleSubmit((data) => console.log(data))}
+            style="primary"
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
